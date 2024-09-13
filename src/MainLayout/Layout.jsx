f4d4873db73c406230a81ebde6806e9490a950ba
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-13 23:41:52 +0600

    add footer  in layout section

--- src/MainLayout/Layout.jsx
+++ src/MainLayout/Layout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Components/Header/Header";
+import Footer from "../Components/Shared/Footer/Footer";
 
 const Layout = () => {
   return (
     <div>
       <Header/>
       <Outlet />
-      <footer>Footer</footer>
+      <Footer/>
     </div>
   );
 };
