4f8422650c79be3a9498589ee272891d3cc57423
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-13 23:39:53 +0600

    fix node problem

--- eslint.config.js
+++ eslint.config.js
@@ -17,6 +17,10 @@ export default [
         sourceType: 'module',
       },
     },
+    "env": {
+    "browser": true,
+    "node": true
+},
     settings: { react: { version: '18.3' } },
     plugins: {
       react,
