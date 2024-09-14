9fa0ad67f964ac2c1601b285175862247ee3f8c7
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-14 22:42:46 +0600

    change class rename to className

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -3,47 +3,47 @@
 const LogIn = () => {
   // const auth = useAuth();
   return (
-    <div class="hero bg-base-200 min-h-screen">
-      <div class="hero-content flex-col lg:flex-row-reverse">
-        <div class="text-center lg:text-left">
-          <h1 class="text-5xl font-bold">Login now!</h1>
-          <p class="py-6">
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
         </div>
-        <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form class="card-body">
-            <div class="form-control">
-              <label class="label">
-                <span class="label-text">Email</span>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
                 placeholder="email"
-                class="input input-bordered"
+                className="input input-bordered"
                 required
               />
             </div>
-            <div class="form-control">
-              <label class="label">
-                <span class="label-text">Password</span>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
               </label>
               <input
                 type="password"
                 placeholder="password"
-                class="input input-bordered"
+                className="input input-bordered"
                 required
               />
-              <label class="label">
-                <a href="#" class="label-text-alt link link-hover">
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
                   Forgot password?
                 </a>
               </label>
             </div>
-            <div class="form-control mt-6">
-              <button class="btn btn-primary">Login</button>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
             </div>
           </form>
         </div>
