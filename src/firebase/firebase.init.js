fbf7d3976765dc184f3779d86dbb8e02d8e86656
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-14 21:58:05 +0600

    hide all secret keys

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrFJnyjI2zu8JD457lWnzE1X9h001agp8",
+  authDomain: "vibespace-1e09f.firebaseapp.com",
+  projectId: "vibespace-1e09f",
+  storageBucket: "vibespace-1e09f.appspot.com",
+  messagingSenderId: "352882258993",
+  appId: "1:352882258993:web:102f8a64495d43c1ee1110"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
