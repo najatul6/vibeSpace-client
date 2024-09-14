e1fb0acf93553d01f4d40ec00b6c952527c7d778
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-14 22:43:10 +0600

    change width

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -3,7 +3,7 @@
 const LogIn = () => {
   // const auth = useAuth();
   return (
-    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero bg-base-200 min-h-screen w-full">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Login now!</h1>
