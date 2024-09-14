eb68babed092d8c5f56c58569cb3bc71cfd6714f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-14 22:45:19 +0600

    change page text

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -6,7 +6,7 @@ const LogIn = () => {
     <div className="hero bg-base-200 min-h-screen w-full">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
+          <h1 className="text-5xl font-bold">Login now !</h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
